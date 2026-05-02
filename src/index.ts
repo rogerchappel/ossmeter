80e1e6bf7ca6fbc7aca72d6ebcec2041be4f21b2
Roger Chappel <miscanalysis@gmail.com>
2026-05-02 18:36:22 +1000

    feat: export public library API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { discoverGitRepositories } from './discover.js';
+export { formatJson, formatMarkdown, formatTable } from './format.js';
+export { scanRepository, scanWorkspace } from './scanner.js';
+export type { QualitySignals, RepositoryMetrics, ScanOptions, ScanReport, WorkspaceSummary } from './models.js';
